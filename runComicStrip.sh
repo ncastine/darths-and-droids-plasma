eabc3e234500a650d5aab7cf24990bdf259c1a70
Neil Castine <neil.castine@gmail.com>
2023-06-25 18:29:59 -0400

    Better script description

--- runComicStrip.sh
+++ runComicStrip.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# For use within a KDE 5 Docker container.
-# Installs our comic and opens the viewer.
+# Entry point for a KDE 5 Docker container. Just runs the Comic Strip app.
 
 # Register the comic that we provided via Docker volume mount
 kpackagetool5 -t Plasma/Comic -i /home/darths/*.comic
