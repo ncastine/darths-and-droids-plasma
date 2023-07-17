01419656fbfddb72c7dcf1cdd7b43d40dd952dc6
Neil Castine <neil.castine@gmail.com>
2023-07-16 20:33:08 -0400

    Group the zero padding tests

--- src/__tests__/main.js
+++ src/__tests__/main.js
@@ -6,24 +6,26 @@
 
 const main = require('../contents/code/main')
 
-/* global expect test */
+/* global describe expect test */
 
-test('when no padding needed', () => {
-  expect(main.zeroPad('1234')).toBe('1234')
-})
+describe('zero padding', () => {
+  test('when no padding needed', () => {
+    expect(main.zeroPad('1234')).toBe('1234')
+  })
 
-test('default padding is 4', () => {
-  expect(main.zeroPad('12')).toBe('0012')
-})
+  test('default padding is 4', () => {
+    expect(main.zeroPad('12')).toBe('0012')
+  })
 
-test('padding specified exactly', () => {
-  expect(main.zeroPad('12', 2)).toBe('12')
-})
+  test('padding specified exactly', () => {
+    expect(main.zeroPad('12', 2)).toBe('12')
+  })
 
-test('default padding undefined input', () => {
-  expect(main.zeroPad()).toBe('0000')
-})
+  test('default padding undefined input', () => {
+    expect(main.zeroPad()).toBe('0000')
+  })
 
-test('default padding null input', () => {
-  expect(main.zeroPad(null)).toBe('0000')
+  test('default padding null input', () => {
+    expect(main.zeroPad(null)).toBe('0000')
+  })
 })
